670657ea52cf2dbd3f6d048abf2e69b2d3722c9c
Daniel Pawlak <daniel@Daniels-iMac.local>
2021-02-13 16:56:41 -0800

    React & Django

---
+++ portfolio_api/frontend/src/components/layout/Header.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react'
+
+export class Header extends Component {
+    render() {
+        return (
+            <nav className="navbar navbar-expand-sm navbar-light bg-light">
+                <div className="container-fluid">
+                    <a className="navbar-brand" href="#">Daniel Pawlak</a>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarNavDropdown">
+                    <ul className="navbar-nav">
+                        <li className="nav-item">
+                        <a className="nav-link active" aria-current="page" href="#">Home</a>
+                        </li>
+                        <li className="nav-item">
+                        <a className="nav-link" href="#">Features</a>
+                        </li>
+                        <li className="nav-item">
+                        <a className="nav-link" href="#">Pricing</a>
+                        </li>
+                        
+                    </ul>
+                    </div>
+                </div>
+            </nav>
+        )
+    }
+}
+
+export default Header
