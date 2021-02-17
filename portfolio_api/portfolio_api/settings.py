69add950f1af825fe3d807b5106e328fbe0175eb
Daniel Pawlak <daniel@Daniels-iMac.local>
2021-02-17 13:45:59 -0800

    Deployment to Heroku

--- portfolio_api/portfolio_api/settings.py
+++ portfolio_api/portfolio_api/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
+django_heroku.settings(locals())
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -24,7 +26,7 @@ SECRET_KEY = 'em2(dm8ls@4@%j7cnf@yq3q$&t-muq_g39skhb$8o2r%u(()k%'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://blooming-citadel-88405.herokuapp.com']
 
 
 # Application definition
