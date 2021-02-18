39c3bb94803cd8db75109f37d5719b2f48c30629
Daniel Pawlak <daniel@Daniels-iMac.local>
2021-02-17 16:35:17 -0800

    Django and Static Assets

--- portfolio_api/portfolio_api/settings.py
+++ portfolio_api/portfolio_api/settings.py
@@ -1,14 +1,8 @@
 import django_on_heroku
 from pathlib import Path
 import os
-# import django_heroku
-# django_heroku.settings(locals())
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'em2(dm8ls@4@%j7cnf@yq3q$&t-muq_g39skhb$8o2r%u(()k%'
@@ -123,5 +117,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
